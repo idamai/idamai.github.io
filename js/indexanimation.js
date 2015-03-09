e67fe60b33ff633a9605f05b72787d1a3ccd7402
Ignatius Damai <ignatiusdamai@gmail.com>
2015-03-10 00:22:35 +0800

    make pages loads without hosting

---
+++ js/indexanimation.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	/*$('#heyiggy-text').css('margin-left',-$(this).width())
+	.css('opacity', 0)
+    .animate({
+        marginLeft:0,
+		opacity:1
+    }, 800);
+	$('#heyiggy-img').css('opacity',0.5)
+    .animate({
+        opacity:1
+    }, 800);*/
+});
