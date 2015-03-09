1d51a5e527940d10a4a3750e4e3d3e858f10218d
Ignatius Damai <ignatiusdamai@gmail.com>
2015-03-10 00:24:58 +0800

    added animation back

--- js/indexanimation.js
+++ js/indexanimation.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	/*$('#heyiggy-text').css('margin-left',-$(this).width())
+	$('#heyiggy-text').css('margin-left',-$(this).width())
 	.css('opacity', 0)
     .animate({
         marginLeft:0,
@@ -8,5 +8,5 @@ $(document).ready(function(){
 	$('#heyiggy-img').css('opacity',0.5)
     .animate({
         opacity:1
-    }, 800);*/
+    }, 800);
 });
